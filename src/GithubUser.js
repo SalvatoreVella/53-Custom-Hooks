7069b6f05e77e5069e5617c0f91ce73f0cceeaf0
Salvatore Vella <salvatorevella1234@gmail.com>
2023-01-09 15:48:21 +0100

    Exercise complete

---
+++ src/GithubUser.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react"
+
+export default function GithubUser({ name }) {
+    const [state, setState] = useState({
+        name: "",
+        id: "",
+        followers: "",
+        repos: ""
+    });
+
+    useEffect(() => {
+        async function fetchData(username) {
+            try {
+                const fetchData = await fetch(`https://api.github.com/users/${username}`);
+                const data = await fetchData.json();
+                setState(() => {
+                    return {
+                        name: data.name,
+                        id: data.id,
+                        followers: data.followers,
+                        repos: data.public_repos
+                    }
+                })
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchData(name)
+    }
+        , [name]);
+    return (
+        <div>
+            <h1>Github Profile: {state.name}</h1>
+            <p>User id: {state.id}</p>
+            <p>Followers: {state.followers}</p>
+            <p>Repos: {state.repos}</p>
+        </div>
+    )
+}
